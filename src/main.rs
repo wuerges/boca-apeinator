13e0308819e332f8bfce21849da47e76569cbc6b
Emilio Wuerges <wuerges@gmail.com>
2024-06-16 18:39:02 -0300

    performed query for runs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-use sqlx::postgres::PgPoolOptions;
 use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::Pool;
+
+async fn select_runs(pool: &Pool<sqlx::Postgres>) -> Result<Vec<(i64,)>, sqlx::Error> {
+    let query = "select * from runtable as r join problemtable as p on r.contestnumber = p.contestnumber join answertable as a on a.answernumber = r.runanswer join usertable as u on u.contestnumber = r.contestnumber order by r.rundatediff desc";
+
+    sqlx::query_as(query).fetch_all(pool).await
+}
 
 #[actix_web::main]
 async fn main() -> Result<(), sqlx::Error> {
@@ -11,15 +18,19 @@ async fn main() -> Result<(), sqlx::Error> {
     //  etc.
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect(&database_url).await?;
+        .connect(&database_url)
+        .await?;
+
+    let rows = select_runs(&pool).await?;
+    println!("rows: {:?}", rows);
 
     // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL/MariaDB)
     let row: (i64,) = sqlx::query_as("SELECT $1")
         .bind(150_i64)
-        .fetch_one(&pool).await?;
+        .fetch_one(&pool)
+        .await?;
 
     assert_eq!(row.0, 150);
 
     Ok(())
 }
-
